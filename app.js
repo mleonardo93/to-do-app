8a904750e0beb461f7e43df893c97edef87909e6
Maya Leonardo <mleonardo@gmail.com>
2017-10-10 03:59:38 -0400

    checkpoint work

--- app.js
+++ app.js
@@ -1,27 +1,68 @@
 const onReady = function() {
+  const toDos = [];
   const addToDoForm = document.getElementById("addToDoForm");
-  const newToDoText = document.getElementById("newToDoText");
-  const toDoList = document.getElementById("toDoList");
 
-  addToDoForm.addEventListener("submit", () => {
-    event.preventDefault();
-    let title = newToDoText.value;
 
-    let newLi = document.createElement("li");
-    let checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    var deleter = document.createElement("input");
-    deleter.type = "button"
-    deleter.value = "Deport"
-    newLi.textContent = title;
-    newLi.appendChild(checkbox);
-    newLi.appendChild(deleter);
-    toDoList.appendChild(newLi);
-    newToDoText.value = "";
-    deleter.onclick = function () {
-      event.currentTarget.parentElement.remove();
+  const createNewToDo = function(){
+    const newToDoText = document.getElementById("newToDoText");
+
+
+//  verbatim from the checkpoint; this didn't work
+//  if (!newToDoText) { return }
+//
+//   this didn't work
+//   if (!newToDoText) {
+//     console.log("no input, aborting function");
+//     return
+//   }
+//
+//   nor did this
+//   !newToDoText ? return;
+
+    toDos.push({
+      title: newToDoText.value,
+      complete: false
+    });
+    newToDoText.value = " ";
+
+    renderTheUI(toDos);
     }
+
+
+  const renderTheUI = function(toDos){
+    const toDoList = document.getElementById("toDoList");
+
+    toDoList.textContent = "";
+
+    toDos.forEach(toDo => {
+      const newLi = document.createElement("li");
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      const deleter = document.createElement("input");
+      deleter.type = "button"
+      deleter.value = "Delete"
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+      newLi.appendChild(deleter);
+
+      deleter.addEventListener("click", function() {
+        console.log(toDo);
+        console.log(toDos.indexOf(toDo));
+        toDos.splice(toDos.indexOf(toDo), 1);
+        renderTheUI(toDos);
+      })
+    })
+  }
+
+  addToDoForm.addEventListener("submit", event => {
+    event.preventDefault();
+    createNewToDo();
+
   });
+
+  renderTheUI(toDos);
 }
 
 window.onload = function() {
